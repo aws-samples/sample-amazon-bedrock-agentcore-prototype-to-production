050d51fb61b6dec63b6ee100cc943b0a6a7abe5a
santoshkulk <santkulk@amazon.com>
2025-09-09 10:52:44 +1000

    model changed to sonnet for runtime and memort

--- 03-agentcore-runtime/strands-runtime/mortgage_agent_runtime.py
+++ 03-agentcore-runtime/strands-runtime/mortgage_agent_runtime.py
@@ -39,7 +39,7 @@ for logger_name in loggers:
 app = BedrockAgentCoreApp()
 
 
-modelID="us.anthropic.claude-3-7-sonnet-20250219-v1:00"
+modelID="us.anthropic.claude-3-7-sonnet-20250219-v1:0"
 
 # get the kb id from AWS SSM
 
